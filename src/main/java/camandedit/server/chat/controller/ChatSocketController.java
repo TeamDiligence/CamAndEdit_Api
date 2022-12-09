5dffa8e39c1b44212b7f9bd91edb07e828cb95e6
inhyeok <ss112d86@gmail.com>
2022-12-09 15:49:14 +0900

    Refactor: 워크스페이스 소켓 채팅 Pub Topic변경

--- src/main/java/camandedit/server/chat/controller/ChatSocketController.java
+++ src/main/java/camandedit/server/chat/controller/ChatSocketController.java
@@ -7,11 +7,13 @@ import camandedit.server.chat.domain.WorkSpaceChat;
 import camandedit.server.global.config.socket.SocketUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.message.SimpleMessage;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -20,14 +22,14 @@ import org.springframework.stereotype.Controller;
 public class ChatSocketController {
 
     private final ChatUseCase chatUseCase;
-    private final SimpMessageSendingOperations messagingTemplate;
+    private final SimpMessagingTemplate messagingTemplate;
 
     @MessageMapping("/workspace/chat/{workSpaceId}")
     public void workspaceChat(@Payload String message, @DestinationVariable("workSpaceId") Long id,
         @Header("simpUser") SocketUser user) {
         WorkSpaceChat chat = chatUseCase.createChat(
             new CreateWorkSpaceChatCommand(user.getUserId(), id, message));
-        messagingTemplate.convertAndSend("/topic/workspace/" + id,
+        messagingTemplate.convertAndSend("/topic/workspace/chat" + id,
             WorkSpaceChatSocketResponse.of(chat));
     }
 }
